7f48f8a405bc48ead1ec8e19ec2579ced5dfec9e
Juan Ruiz <juancaruiz@Juancas-MacBook-Pro.local>
2021-07-06 22:25:41 -0700

    built home page

--- synthesis-app/components/NavBar.js
+++ synthesis-app/components/NavBar.js
@@ -5,7 +5,9 @@ import styles from "../styles/NavBar.module.css";
 const NavBar = () => {
   return (
     <nav className={styles.mainNav}>
-      <Image src="/synthesis-logo.svg" width={80} height={30} />
+      <Link href="/">
+        <Image src="/synthesis-logo.svg" width={80} height={30} />
+      </Link>
       <Link href="/about">
         <a className={styles.about}>About Me & Why Synthesis</a>
       </Link>
